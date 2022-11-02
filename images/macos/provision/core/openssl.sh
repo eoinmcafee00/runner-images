a67a0baaede0d66f01288fa6a75bf11f4444864f
Eoin McAfee <eoin.mcafee@harness.io>
2022-11-02 11:55:32 +0000

    t

--- images/macos/provision/core/openssl.sh
+++ images/macos/provision/core/openssl.sh
@@ -5,9 +5,9 @@ echo "Install openssl@1.1"
 brew_smart_install "openssl@1.1"
 
 # Symlink brew openssl@1.1 to `/usr/local/bin` as Homebrew refuses
-sudo ln -sf $(brew --prefix openssl@1.1)/bin/openssl /usr/local/bin/openssl
+#sudo ln -sf $(brew --prefix openssl@1.1)/bin/openssl /usr/local/bin/openssl
 
 # Most of buildsystems and scripts look up ssl here
-sudo ln -sf $(brew --cellar openssl@1.1)/1.1* /usr/local/opt/openssl
+#sudo ln -sf $(brew --cellar openssl@1.1)/1.1* /usr/local/opt/openssl
 
-invoke_tests "OpenSSL"
+#invoke_tests "OpenSSL"
