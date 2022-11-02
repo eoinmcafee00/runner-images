d5cda5a6d5d089b132d341b9f9cf2bae90da9924
Eoin McAfee <eoin.mcafee@harness.io>
2022-11-02 15:14:49 +0000

    try

--- images/macos/provision/core/rubygem.sh
+++ images/macos/provision/core/rubygem.sh
@@ -1,6 +1,15 @@
 #!/bin/bash -e -o pipefail
 source ~/utils/utils.sh
 
+brew install chruby ruby-install
+brew config
+ruby-install ruby -- --enable-shared
+ruby-install ruby
+
+echo "source $(brew --prefix)/opt/chruby/share/chruby/chruby.sh" >> ~/.bashrc
+echo "source $(brew --prefix)/opt/chruby/share/chruby/auto.sh" >> ~/.bashrc
+echo "chruby ruby-3.1.2" >> ~/.bashrc
+
 echo Updating RubyGems...
 gem update --system
 
@@ -11,10 +20,6 @@ if [ -n "$gemsToInstall" ]; then
         gem install $gem
     done
 fi
-
-export GEM_HOME=~/.gem >> "$HOME/.bashrc"
-export GEM_PATH=~/.gem >> "$HOME/.bashrc"
-export PATH=$PATH:$HOME/.gem/bin >> "$HOME/.bashrc"
 
 # Temporary uninstall public_suffix 5.0 gem as Cocoapods is not compatible with it yet https://github.com/actions/runner-images/issues/6149
 gem uninstall public_suffix -v 5.0.0
