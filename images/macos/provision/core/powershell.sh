5253e45690c7176626d7853c31d58b7a70eb5990
Eoin McAfee <eoin.mcafee@harness.io>
2022-11-02 13:44:42 +0000

    remove testing

--- images/macos/provision/core/powershell.sh
+++ images/macos/provision/core/powershell.sh
@@ -33,7 +33,4 @@ pwsh -command "& {Import-Module Az}"
 # powershell link was removed in powershell-6.0.0-beta9
 sudo ln -s /usr/local/bin/pwsh /usr/local/bin/powershell
 
-sudo ln -s /opt/homebrew/bin/python3 /usr/local/bin/python3
-
-
 invoke_tests "Powershell"
